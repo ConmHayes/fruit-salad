af18921bd6217faa135a0fb38fd9d7b3293a1291
Connor Hayes <conmhayes@gmail.com>
2023-09-11 15:02:21 +0100

    fetching data

--- js/script.js
+++ js/script.js
@@ -2,10 +2,46 @@ const fruitForm = document.querySelector("#inputSection form")
 
 const fruitList = document.querySelector("#fruitSection ul")
 
+const fruitNutrition = document.querySelector("#nutritionSection p")
+
+const fetchFruitData = fruit => {
+    fetch(`https://fruity-api.onrender.com/api/fruits/${fruit}`)
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data)
+            addFruit(data)
+        })
+
+        .catch(e => console.error(e))
+}
+
+let totalCal = 0,
+    totalCarbs = 0,
+    totalFat = 0,
+    totalPro = 0,
+    totalSugar = 0
+
+
 const addFruit = fruit => {
     const li = document.createElement("li")
-    li.textContent = fruit
+    li.textContent = fruit.name
     fruitList.appendChild(li)
+
+    totalCal += fruit.nutritions.calories
+    //fruitNutrition.textContent = `${totalCal} Calories \n${totalCarbs} Carbohydrates \n${totalFat} Fat \n${totalPro} Protein \n${totalSugar} Sugar`
+
+    totalCarbs += fruit.nutritions.carbohydrates
+    //fruitNutrition.textContent = `${totalCarbs} Carbohydrates`
+
+    totalFat += fruit.nutritions.fat
+    //fruitNutrition.textContent = `${totalFat} Fat`
+
+    totalPro += fruit.nutritions.protein
+    //fruitNutrition.textContent = `${totalPro} Protein`
+
+    totalSugar += fruit.nutritions.sugar
+    //fruitNutrition.textContent = `${totalSugar} Sugar`
+    fruitNutrition.textContent = `${totalCal.toFixed(2)} Calories \n ${totalCarbs.toFixed(2)} Carbohydrates \n${totalFat.toFixed(2)} Fat \n${totalPro.toFixed(2)} Protein \n${totalSugar.toFixed(2)} Sugar`
 }
 
 const removeFruit = fruit => {
@@ -14,7 +50,7 @@ const removeFruit = fruit => {
 
 fruitForm.addEventListener("submit", e => {
     e.preventDefault()
-    addFruit(e.target.fruitInput.value)
+    fetchFruitData(e.target.fruitInput.value)
     e.target.fruitInput.value = ""
 })
 
